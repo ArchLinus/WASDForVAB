0502262dbade140a25c62666661a399c6d152333
ArchLinus <archlinus123@gmail.com>
2023-02-27 21:35:12 -0500

    Initial

---
+++ WASDForVab/WASDMod.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using BepInEx;
+using I2.Loc;
+using KSP.Game;
+using KSP.Modules;
+using KSP.OAB;
+using KSP.Sim;
+using UnityEngine;
+using HarmonyLib;
+using KSP.Messages;
+using System.Reflection;
+
+namespace KSPTestMod
+{
+    [BepInPlugin("io.bepis.wasd_for_vab", "WASD for VAB", "1.0")]
+    public class WASDMod : BaseUnityPlugin
+    {
+        public GameInstance Game => GameManager.Instance.Game;
+
+        private static Harmony harmony;
+
+        private SubscriptionHandle loadSubscription;
+
+        public void Awake()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            harmony = Harmony.CreateAndPatchAll(assembly);
+            WASDPatches.patchState.Logger = Logger;
+            loadSubscription = Game.Messages.Subscribe<OABLoadFinalizedMessage>(OnOABLoadFinalized);
+        }
+
+        private void OnOABLoadFinalized(MessageCenterMessage msg)
+        {
+            if ((msg as OABLoadFinalizedMessage).isSnapshotLoaded)
+            {
+                ObjectAssemblyBuilder current = Game.OAB.Current;
+                WASDPatches.patchState.OnLoad(current.CameraManager);
+            }
+        }
+
+        public void Update()
+        {
+            if (Game != null)
+            {
+                if (Game.OAB != null && Game.OAB.IsLoaded)
+                {
+                    if (Input.GetMouseButton(1))
+                    {
+                        Vector3d inputVector = new Vector3d();
+                        if (Input.GetKey(KeyCode.W))
+                        {
+                            inputVector.z = 1;
+                        }
+                        if (Input.GetKey(KeyCode.S))
+                        {
+                            inputVector.z = -1;
+                        }
+                        if (Input.GetKey(KeyCode.D))
+                        {
+                            inputVector.x = 1;
+                        }
+                        if (Input.GetKey(KeyCode.A))
+                        {
+                            inputVector.x = -1;
+                        }
+                        if (Input.GetKey(KeyCode.E))
+                        {
+                            inputVector.y = 1;
+                        }
+                        if (Input.GetKey(KeyCode.Q))
+                        {
+                            inputVector.y = -1;
+                        }
+
+                        if (Input.GetKey(KeyCode.LeftShift))
+                        {
+                            inputVector *= 2;
+                        }
+
+                        if (!inputVector.IsZero())
+                        {
+                            WASDPatches.patchState.OnMove(inputVector, Time.deltaTime);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void OnDestroy()
+        {
+            Game.Messages.Unsubscribe(ref loadSubscription);
+            harmony.UnpatchSelf();
+        }
+
+    }
+}
---
+++ WASDForVab/WASDPatches.cs
@@ -0,0 +1,81 @@
+using KSP.OAB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace KSPTestMod
+{
+    public class WASDPatches
+    {
+        public class WASDPatchesState
+        {
+            public float yaw = 0;
+            public float pitch = 0;
+            public Vector3d lookDirection;
+            public ManualLogSource Logger;
+            public float movementSpeed = 20.5f;
+            public ObjectAssemblyCameraManager cameraManager;
+            public Vector3d cameraPos = new Vector3d();
+
+            public void OnLoad(ObjectAssemblyCameraManager cameraManager)
+            {
+                this.cameraManager = cameraManager;
+                pitch = cameraManager.CameraGimbal.transform.rotation.eulerAngles.x;
+                yaw = cameraManager.CameraGimbal.transform.rotation.eulerAngles.z;
+                Logger.LogInfo("Pitch: " + pitch);
+                Logger.LogInfo("Yaw: " + yaw);
+            }
+
+            public void OnMove(Vector3d inputVector, float deltaTime)
+            {
+                if (cameraManager != null)
+                {
+                    Vector3d forward = cameraManager.gimbalTransform.transform.forward;
+                    Vector3d right = -Vector3d.Cross(forward, Vector3d.up);
+                    Vector3d up = Vector3d.up;
+                    Vector3d movement = forward * inputVector.z + right * inputVector.x + up * inputVector.y;
+                    movement *= deltaTime * movementSpeed;
+                    cameraManager.gimbalTransform.position = cameraManager.gimbalTransform.position + movement;
+                }
+            }
+        }
+
+        public static WASDPatchesState patchState = new WASDPatchesState();
+
+        [HarmonyPatch(typeof(ObjectAssemblyPlacementTool), "ProcessInputCameraRotation")]
+        class ProcessInputCameraRotationPatch
+        {
+            static bool Prefix(Vector3 orbitTargetPos, float prevYaw, float prevPitch, float deltaYaw, float deltaPitch, float distance, ref Quaternion lookRotation, ref Vector3 lookDirection, ref Vector3 lookPosition)
+            {
+                patchState.Logger.LogInfo("Euler: " + patchState.cameraManager.gimbalTransform.transform.eulerAngles);
+                var currentPitch = patchState.cameraManager.gimbalTransform.transform.eulerAngles.x;
+                var currentYaw = patchState.cameraManager.gimbalTransform.transform.eulerAngles.y;
+                var newPitch = currentPitch + deltaPitch;
+                if (currentPitch > 180.0f && currentPitch + deltaPitch < 270.0f)
+                {
+                    newPitch = 270.0f;
+                }
+                else if (currentPitch < 90.0f && currentPitch + deltaPitch > 90.0f)
+                {
+                    newPitch = 90.0f;
+                }
+                var newYaw = currentYaw + deltaYaw;
+                patchState.Logger.LogInfo("Pitch: " + newPitch);
+                patchState.Logger.LogInfo("Yaw: " + newYaw);
+                // patchState.yaw += deltaYaw;
+                // patchState.pitch += deltaPitch;
+                Vector3d lookDir = new Vector3d(0, 0, 1);
+                lookRotation = QuaternionD.AngleAxis(newYaw, Vector3d.up) * QuaternionD.AngleAxis(newPitch, Vector3d.right);
+                lookDir = lookRotation * lookDir;
+                lookDirection = lookDir;
+                lookPosition = patchState.cameraManager.gimbalTransform.position;
+                return false;
+            }
+        };
+    }
+}
