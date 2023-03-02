a01435f1512429e00c5c9958a6f99733f8e2fea6
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-03-02 23:57:27 +0100

    Add bindings to SpaceWarp config
    - bindings are shown in config, can be modified by users
    - require right click for control can be toggled off in config

--- WASDForVab/WASDConfig.cs
+++ WASDForVab/WASDConfig.cs
@@ -22,5 +22,41 @@ namespace WASDForVAB
         [ConfigField("SlowSpeedMultiplier")]
         [ConfigDefaultValue(0.4f)]
         public float SlowSpeedMultiplier = 0.4f;
+
+        [ConfigField("RequireRightClickForControl")]
+        [ConfigDefaultValue(true)]
+        public bool RequireRightClickForControl = true;
+
+        [ConfigField("KeyForward")]
+        [ConfigDefaultValue("W")]
+        public string KeyForward = "W";
+
+        [ConfigField("KeyBack")]
+        [ConfigDefaultValue("S")]
+        public string KeyBack = "S";
+
+        [ConfigField("KeyRight")]
+        [ConfigDefaultValue("D")]
+        public string KeyRight = "D";
+
+        [ConfigField("KeyLeft")]
+        [ConfigDefaultValue("A")]
+        public string KeyLeft = "A";
+
+        [ConfigField("KeyUp")]
+        [ConfigDefaultValue("E")]
+        public string KeyUp = "E";
+
+        [ConfigField("KeyDown")]
+        [ConfigDefaultValue("Q")]
+        public string KeyDown = "Q";
+
+        [ConfigField("KeyFast")]
+        [ConfigDefaultValue("LEFT SHIFT")]
+        public string KeyFast = "LEFT SHIFT";
+
+        [ConfigField("KeySlow")]
+        [ConfigDefaultValue("LEFT CTRL")]
+        public string KeySlow = "LEFT CTRL";        
     }
 }
